1a8262c1e49814efa4bb7c794997bf6f1661712b
MahdiWidian <maws.production@gmail.com>
2023-12-18 23:33:15 +0700

    feat : jadi nih

---
+++ config.js
@@ -0,0 +1,48 @@
+// Copied from: https://github.com/AJLoveChina/birthday
+var config = {
+    // You can have any number of sentences here, you can write ten or twenty sentences
+    // Try to keep each sentence within 15 words, otherwise the display effect may not be good
+    texts: [
+        "Hawlooo cantikkk,",      
+        "Ciee hari ini ulang tahun yaa,",  
+        "Pantesss papnya tadi bersinar bangettt, mendekati hari bahagia, barakallah fii umrik ya Farah Cantik,",
+        "doa aku dari dulu ga berubah dan selalu istiqomah yaitu",
+        "pastinya panjang umur, rezeki serta mulia, bisa jadi anak yang dibanggakan orang tua,",
+        "tercapai segala tujuan positifnya, diberikan kesabaran dan kekuatan dalam segala hal,",
+        "dan ketika sudah merasa lelah ya diberikan kemudahan dalam segala urusan dunia maupun akhirat,",
+        "semoga juga bisa makin sholeh, shalat duhanya",
+        "shalat sunahnya, baca alqurannya senantiasa selalu diingat",
+        "dan selalu sayang kepada kedua orang tuanya, berbakti jugaa",
+        "dan jangan lupaa kamu punyaa pacar",
+        "jadi.......",
+        "tolong tetap semangat ya menjalin hubungan dengan ku hihi ^__^",
+        "makin berkembang hubungannya",
+        "semakin dekat dengan halal aamiin",
+        "sisanyaa aku sebutkan doanya dalam diam yaa hehe",
+        "i love you so much ❤️❤️❤️",
+    ],
+    /**
+     * You can leave the "imgs" field empty, but if you want to fill it, it must follow the format below
+     * "The corresponding sentence, must be exactly the same": "Image address, you can put the image in the imgs folder"
+     * For example
+     * "my beloved sweetheart": "./imgs/xiaokeai.jpg"
+     *
+     * If you don't want an image, just put two slashes at the beginning of each line as a comment, like the image for "Today is your birthday" won't be displayed :)
+     * Tip: It's better to use square or nearly square images for better visual effect
+     */
+    imgs: {
+        "my beloved sweetheart": "./imgs/xiaokeai.png",
+        // "Today is your birthday": "./imgs/birthday.jpg",
+    },
+    // Button text description, below are the default button texts in English, you can change them to your preferred text
+    desc: {
+        turn_on: "Hayuuu Mulaii",
+        play: "Manaaa Musicnyaa.....",
+        bannar_coming: "Tarak tak dunggg.......",
+        balloons_flying: "Adaaa yangg kurangg nihhh",
+        cake_fadein: "Kueee gaaa?",
+        light_candle: "Lilinn kali yaa?",
+        wish_message: "Barakallah Fii Umrikk",
+        story: "A MESSAGE FOR YOU",
+    }
+};
